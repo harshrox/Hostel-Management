09ed30cabc03bb799a24c3e152c7c1ec605d4ab7
Harsh Anand <harshjha08@outlook.com>
2025-10-23 01:22:12 +0530

    Added role based checking

--- Backend/accounts/views.py
+++ Backend/accounts/views.py
@@ -1,35 +1,36 @@
-from rest_framework import viewsets, permissions, generics
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import viewsets, generics
 from .models import User
 from .serializers import UserSerializer
-
-# JWT imports
+from .permissions import IsAdminRole
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-
-
+# -------------------------------
 # User CRUD (Admin only)
+# -------------------------------
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAdminUser]  # only admins
+    permission_classes = [IsAdminRole]  # only role='ADMIN'
 
     def get_queryset(self):
         return User.objects.all()
 
 
-
+# -------------------------------
 # User Registration
+# -------------------------------
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [AllowAny]
 
 
+# -------------------------------
 # User Profile (current logged-in user)
+# -------------------------------
 class UserProfileView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
     def get_object(self):
         return self.request.user

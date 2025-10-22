1f012f02582e926c22fe5d9cf9ce3371c00072b8
Harsh Anand <harshjha08@outlook.com>
2025-10-23 00:44:36 +0530

    Updated room serializer

--- Backend/rooms/serializers.py
+++ Backend/rooms/serializers.py
@@ -3,9 +3,19 @@ from .models import Room, Allocation
 from accounts.serializers import UserSerializer
 
 class RoomSerializer(serializers.ModelSerializer):
+    allocated_count = serializers.SerializerMethodField()
+    is_allocated = serializers.SerializerMethodField()
+
     class Meta:
         model = Room
-        fields = ['id', 'number', 'room_type', 'capacity']
+        fields = ['id', 'number', 'room_type', 'capacity', 'allocated_count', 'is_allocated']
+
+    def get_allocated_count(self, obj):
+        return obj.allocated_count
+
+    def get_is_allocated(self, obj):
+        return obj.is_allocated
+
 
 class AllocationSerializer(serializers.ModelSerializer):
     student = UserSerializer(read_only=True)

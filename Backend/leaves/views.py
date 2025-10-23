8bd93dc258eb566aa2221b6d81e3e9a02415b046
Harsh Anand <harshjha08@outlook.com>
2025-10-23 12:55:16 +0530

    Added role based leave view

--- Backend/leaves/views.py
+++ Backend/leaves/views.py
@@ -1,8 +1,30 @@
 from rest_framework import viewsets, permissions
 from .models import Leave
 from .serializers import LeaveSerializer
+from .permissions import IsWardenRole
 
 class LeaveViewSet(viewsets.ModelViewSet):
     queryset = Leave.objects.all()
     serializer_class = LeaveSerializer
-    permission_classes = [permissions.IsAuthenticated]
+
+    def get_permissions(self):
+        """
+        Warden: full CRUD
+        Student: can create and view their own leaves
+        """
+        if self.action in ['list', 'retrieve', 'create']:
+            permission_classes = [permissions.IsAuthenticated]
+        else:
+            permission_classes = [IsWardenRole]
+        return [perm() for perm in permission_classes]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.role == 'WARDEN':
+            return Leave.objects.all()
+        else:
+            return Leave.objects.filter(student=user)
+
+    def perform_create(self, serializer):
+        # Automatically assign the logged-in student
+        serializer.save(student=self.request.user)

c4835129760559451a84750a7ebe6edfab23acd5
Harsh Anand <harshjha08@outlook.com>
2025-10-23 13:09:35 +0530

    Updated records url

--- Backend/records/urls.py
+++ Backend/records/urls.py
@@ -2,6 +2,6 @@ from rest_framework import routers
 from .views import StudentRecordViewSet
 
 router = routers.DefaultRouter()
-router.register(r'student-records', StudentRecordViewSet)
+router.register(r'records', StudentRecordViewSet)
 
 urlpatterns = router.urls

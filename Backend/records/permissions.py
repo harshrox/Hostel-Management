66d68ec60551e9e92f7e19fd430e85d53a3ed82c
Harsh Anand <harshjha08@outlook.com>
2025-10-23 13:07:04 +0530

    Added permission based record handling

---
+++ Backend/records/permissions.py
@@ -0,0 +1,6 @@
+from rest_framework import permissions
+
+class IsWardenRole(permissions.BasePermission):
+    """Allows access only to users with role='WARDEN'."""
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_authenticated and request.user.role == 'WARDEN')

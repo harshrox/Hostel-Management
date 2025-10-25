acf105c0eab4f51e61e2b3c49aa36becbaa8e20a
Harsh Anand <harshjha08@outlook.com>
2025-10-25 13:19:30 +0530

    Updated Routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,31 +1,39 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
-import ProtectedRoute from "./components/Layout/ProtectedRoute";
-
+import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Auth/Login";
-import Register from "./pages/Auth/Register";
-import Profile from "./pages/Auth/Profile";
+import WardenDashboard from "./pages/Warden/Dashboard";
+import StudentDashboard from "./pages/Student/Dashboard";
+import UserList from "./pages/Warden/Users/UserList";
+import AddUser from "./pages/Warden/Users/AddUser";
+import ProtectedRoute from "./components/Layout/ProtectedRoute";
 
 function App() {
   return (
-    <BrowserRouter>
-      <AuthProvider>
-        <Routes>
-          <Route path="/" element={<Navigate to="/login" replace />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+    <Routes>
+      <Route path="/login" element={<Login />} />
 
-          <Route
-            path="/profile"
-            element={
-              <ProtectedRoute>
-                <Profile />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
-      </AuthProvider>
-    </BrowserRouter>
+      {/* Warden Routes */}
+      <Route
+        path="/warden/dashboard"
+        element={<ProtectedRoute role="WARDEN"><WardenDashboard /></ProtectedRoute>}
+      />
+      <Route
+        path="/warden/users"
+        element={<ProtectedRoute role="WARDEN"><UserList /></ProtectedRoute>}
+      />
+      <Route
+        path="/warden/users/add"
+        element={<ProtectedRoute role="WARDEN"><AddUser /></ProtectedRoute>}
+      />
+
+      {/* Student Routes */}
+      <Route
+        path="/student/dashboard"
+        element={<ProtectedRoute role="STUDENT"><StudentDashboard /></ProtectedRoute>}
+      />
+
+      {/* Default */}
+      <Route path="*" element={<Login />} />
+    </Routes>
   );
 }
 

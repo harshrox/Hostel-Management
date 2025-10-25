2b6092a5737bd3042c13449fb3de68f5d149e6c9
Harsh Anand <harshjha08@outlook.com>
2025-10-25 23:13:11 +0530

    Configued for room allocation

--- Backend/rooms/serializers.py
+++ Backend/rooms/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import Room, Allocation
+from accounts.models import User
 from accounts.serializers import UserSerializer
+
 
 class RoomSerializer(serializers.ModelSerializer):
     allocated_count = serializers.SerializerMethodField()
@@ -18,8 +20,16 @@ class RoomSerializer(serializers.ModelSerializer):
 
 
 class AllocationSerializer(serializers.ModelSerializer):
+    # For read (GET) requests → show full student info
     student = UserSerializer(read_only=True)
+    # For write (POST/PUT) requests → accept student and room IDs
+    student_id = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.filter(role='STUDENT'), source='student', write_only=True
+    )
+    room_id = serializers.PrimaryKeyRelatedField(
+        queryset=Room.objects.all(), source='room', write_only=True
+    )
 
     class Meta:
         model = Allocation
-        fields = ['id', 'student', 'room', 'start_date', 'end_date']
+        fields = ['id', 'student', 'student_id', 'room', 'room_id', 'start_date', 'end_date']

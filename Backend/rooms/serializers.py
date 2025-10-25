f67a0ed0f20c8d0c1e604d0681cd12d211fc0e29
Harsh Anand <harshjha08@outlook.com>
2025-10-25 23:57:16 +0530

    Configured room allocation serializer

--- Backend/rooms/serializers.py
+++ Backend/rooms/serializers.py
@@ -24,12 +24,22 @@ class RoomSerializer(serializers.ModelSerializer):
 class AllocationSerializer(serializers.ModelSerializer):
     student_id = serializers.IntegerField(write_only=True)
     room_id = serializers.IntegerField(write_only=True)
-    student = serializers.StringRelatedField(read_only=True)
-    room = serializers.StringRelatedField(read_only=True)
+    
+    # Read-only fields for the frontend table
+    student_username = serializers.CharField(source="student.username", read_only=True)
+    room_number = serializers.CharField(source="room.number", read_only=True)
 
     class Meta:
         model = Allocation
-        fields = ['id', 'student', 'room', 'start_date', 'end_date', 'student_id', 'room_id']
+        fields = [
+            'id',
+            'student_username',
+            'room_number',
+            'start_date',
+            'end_date',
+            'student_id',
+            'room_id',
+        ]
 
     def create(self, validated_data):
         student_id = validated_data.pop('student_id')
@@ -38,4 +48,5 @@ class AllocationSerializer(serializers.ModelSerializer):
         room = Room.objects.get(id=room_id)
         allocation = Allocation.objects.create(student=student, room=room, **validated_data)
         return allocation
+
 

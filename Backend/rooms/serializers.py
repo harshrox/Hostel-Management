3892b331fc934a94e145877734cdd69d4ca00861
Harsh Anand <harshjha08@outlook.com>
2025-10-26 00:21:21 +0530

    Fixed end_date less than start_date issue

--- Backend/rooms/serializers.py
+++ Backend/rooms/serializers.py
@@ -3,7 +3,6 @@ from .models import Room, Allocation
 from accounts.models import User
 from accounts.serializers import UserSerializer
 
-
 class RoomSerializer(serializers.ModelSerializer):
     allocated_count = serializers.SerializerMethodField()
     is_allocated = serializers.SerializerMethodField()
@@ -17,8 +16,6 @@ class RoomSerializer(serializers.ModelSerializer):
 
     def get_is_allocated(self, obj):
         return obj.is_allocated
-
-
 
 
 class AllocationSerializer(serializers.ModelSerializer):
@@ -41,6 +38,15 @@ class AllocationSerializer(serializers.ModelSerializer):
             'room_id',
         ]
 
+    def validate(self, attrs):
+        start_date = attrs.get('start_date')
+        end_date = attrs.get('end_date')
+        if end_date and start_date and end_date < start_date:
+            raise serializers.ValidationError({
+                "end_date": "End date cannot be before start date."
+            })
+        return attrs
+
     def create(self, validated_data):
         student_id = validated_data.pop('student_id')
         room_id = validated_data.pop('room_id')
@@ -49,4 +55,20 @@ class AllocationSerializer(serializers.ModelSerializer):
         allocation = Allocation.objects.create(student=student, room=room, **validated_data)
         return allocation
 
+    def update(self, instance, validated_data):
+        start_date = validated_data.get('start_date', instance.start_date)
+        end_date = validated_data.get('end_date', instance.end_date)
+        if end_date and end_date < start_date:
+            raise serializers.ValidationError({
+                "end_date": "End date cannot be before start date."
+            })
 
+        # Update fields
+        instance.start_date = start_date
+        instance.end_date = end_date
+        if 'student_id' in validated_data:
+            instance.student = User.objects.get(id=validated_data['student_id'])
+        if 'room_id' in validated_data:
+            instance.room = Room.objects.get(id=validated_data['room_id'])
+        instance.save()
+        return instance

f8a9154ba6ef93b7911f47b5dcdbc02c12c2166f
Harsh Anand <harshjha08@outlook.com>
2025-10-26 01:47:18 +0530

    Fixed time format issue

--- Backend/complaints/serializers.py
+++ Backend/complaints/serializers.py
@@ -4,9 +4,7 @@ from accounts.serializers import UserSerializer
 
 class ComplaintSerializer(serializers.ModelSerializer):
     student = UserSerializer(read_only=True)
-    created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", read_only=True)
-    updated_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", read_only=True)
-
+    
     class Meta:
         model = Complaint
         fields = [

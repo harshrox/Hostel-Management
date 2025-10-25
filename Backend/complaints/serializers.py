c3bcef49b57bd3bcdeb872990791531c6d42b960
Harsh Anand <harshjha08@outlook.com>
2025-10-26 01:00:24 +0530

    Updated complaints serializer to return time

--- Backend/complaints/serializers.py
+++ Backend/complaints/serializers.py
@@ -4,7 +4,17 @@ from accounts.serializers import UserSerializer
 
 class ComplaintSerializer(serializers.ModelSerializer):
     student = UserSerializer(read_only=True)
+    created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", read_only=True)
+    updated_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", read_only=True)
 
     class Meta:
         model = Complaint
-        fields = ['id', 'student', 'title', 'description', 'status', 'created_at', 'updated_at']
+        fields = [
+            'id', 
+            'student', 
+            'title', 
+            'description', 
+            'status', 
+            'created_at', 
+            'updated_at'
+        ]

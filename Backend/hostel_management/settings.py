5217818e0f47908627034df2cc7c8b6fd7145c87
Harsh Anand <harshjha08@outlook.com>
2025-10-25 12:42:31 +0530

    Configured cors header settings

--- Backend/hostel_management/settings.py
+++ Backend/hostel_management/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'complaints',
     'leaves',
     'records',
+    'corsheaders'
 ]
 
 # Use the custom User model
@@ -54,6 +55,7 @@ AUTH_USER_MODEL = 'accounts.User'
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -64,6 +66,13 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'hostel_management.urls'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
 
 TEMPLATES = [
     {
